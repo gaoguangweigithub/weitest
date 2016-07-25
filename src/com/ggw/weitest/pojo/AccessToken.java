195ab3289877a7a0281efe0546677d26eab6e913
gaoguangweigithub <454809244@qq.com>
2016-07-25 19:30:21 +0800

    测试

--- src/com/ggw/weitest/pojo/AccessToken.java
+++ src/com/ggw/weitest/pojo/AccessToken.java
@@ -2,21 +2,25 @@ package com.ggw.weitest.pojo;
 
 public class AccessToken {
 
-	// 获取到的凭证  
-    private String token;  
-    // 凭证有效时间，单位：秒  
-    private int expiresIn;
+	// 获取到的凭证1
+	private String token;
+	// 凭证有效时间，单位：秒
+	private int expiresIn;
+
 	public String getToken() {
 		return token;
 	}
+
 	public void setToken(String token) {
 		this.token = token;
 	}
+
 	public int getExpiresIn() {
 		return expiresIn;
 	}
+
 	public void setExpiresIn(int expiresIn) {
 		this.expiresIn = expiresIn;
-	}  
-    
+	}
+
 }
